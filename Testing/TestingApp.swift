b24a82382fdfe942f3d3464c2fb66b9726eda5cb
NSightx <NSightres@gmail.com>
2024-02-05 12:31:35 +0300

    Initial Commit

---
+++ Testing/TestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingApp.swift
+//  Testing
+//
+//  Created by Nsight on 02.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
